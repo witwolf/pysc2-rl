15568b4e7a20a855cb141c217d933a14f569e104
witwolf <yingxiang.hong@hobot.cc>
2018-11-01 16:51:30 +0800

    remove protoss_agent.py

--- tests/test_protoss_macro.py
+++ tests/test_protoss_macro.py
@@ -4,10 +4,80 @@
 
 from pysc2.tests import utils
 from absl.testing import absltest
-from lib.protoss_macro import PROTOSS_MACROS
 from pysc2.env import sc2_env
 from pysc2.lib import features
-from algorithm.script.protoss_agent import ProtossAgent
+from pysc2.agents import base_agent
+from pysc2.lib import units
+from pysc2.lib.actions import FUNCTIONS
+from lib.protoss_macro import PROTOSS_MACROS
+from lib.protoss_macro import U
+
+
+class ProtossAgent(base_agent.BaseAgent):
+    def __init__(self):
+        super(ProtossAgent, self).__init__()
+        self.actions = []
+        self.aid = 0
+        self.frame = 0
+
+    def get_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.feature_units
+                if unit.unit_type == unit_type]
+
+    def get_all_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type]
+
+    def get_all_complete_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.raw_units
+                if unit.unit_type == unit_type and int(unit.build_progress) == 100]
+
+    def get_unit_counts(self, obs, unit_type):
+        for unit in obs.observation.unit_counts:
+            if unit[0] == unit_type:
+                return unit[1]
+        return 0
+
+    def can_do(self, obs, action):
+        return action in obs.observation.available_actions
+
+    def future_food(self, obs):
+        nexus = len(self.get_all_units_by_type(obs, units.Protoss.Nexus))
+        pylons = len(self.get_all_units_by_type(obs, units.Protoss.Pylon))
+        return 15 * (nexus) + 8 * (pylons)
+
+    def step(self, obs):
+        super(ProtossAgent, self).step(obs)
+        if obs.first():
+            U.base_minimap_location(obs)
+            U.enemy_minimap_location(obs)
+
+        self.frame += 1
+        if self.frame % 4 != 1:
+            return FUNCTIONS.no_op()
+        # find macro to execute
+        if len(self.actions) == 0:
+            # if enough food, train probe
+            mineral = obs.observation.player.minerals
+            food = self.future_food(obs) - obs.observation.player.food_used
+            if mineral > 50 and food > 1:
+                self.actions = PROTOSS_MACROS.Train_Probe()
+            # build a pylon if not enough food
+            elif mineral > 100:
+                self.actions = PROTOSS_MACROS.Build_Pylon()
+        # execute action
+        else:
+            # if macro execute end, clear action
+            if self.aid == len(self.actions):
+                self.aid, self.actions = 0, []
+            else:
+                # execute one step macro
+                action, actions_args_func = self.actions[self.aid]
+                action_args = actions_args_func(obs)
+                if self.can_do(obs, action.id):
+                    self.aid += 1
+                    return action(*action_args)
+        return FUNCTIONS.no_op()
 
 
 class TestProtossMacro(utils.TestCase):
